eeb2ae278954bbee0e60b892758842ceb576aed1
manci <javiercaliescali@gmail.com>
2020-08-05 18:28:33 -0500

    create data object with info, and create dynamic products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from './data'
 import './App.css';
 
 function App() {
@@ -40,50 +41,21 @@ function App() {
     <main className="main">
       <div className="content">
         <ul className="products">
-          <li>
-            <div className="product">
-              <img className="product-image" src="/images/d1.jpeg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt</a>
+          {
+            data.products.map(product =>           
+            <li>
+              <div className="product">
+                <img className="product-image" src={product.image} alt="product" />
+                <div className="product-name">
+                  <a href="product.html">{product.name}</a>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">{product.price}</div>
+                <div className="product-rating">{product.rating} Stars ({product.numReviews} reviews)</div>
               </div>
-              <div className="product-brand">Nike</div>
-              <div className="product-price">$60</div>
-              <div className="product-rating">4.5 Stars (10 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="/images/d1.jpeg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt</a>
-              </div>
-              <div className="product-brand">Nike</div>
-              <div className="product-price">$60</div>
-              <div className="product-rating">4.5 Stars (10 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="/images/d1.jpeg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt</a>
-              </div>
-              <div className="product-brand">Nike</div>
-              <div className="product-price">$60</div>
-              <div className="product-rating">4.5 Stars (10 reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="/images/d1.jpeg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt</a>
-              </div>
-              <div className="product-brand">Nike</div>
-              <div className="product-price">$60</div>
-              <div className="product-rating">4.5 Stars (10 reviews)</div>
-            </div>
-          </li>         
+            </li>)
+          }
+           
         </ul>
       </div>
     </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+export default {
+  products: [
+    {
+      name: 'Slim shirt',
+      category: 'shirts',
+      image: '/images/d1.jpeg',
+      price: 55,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: ' Fit shirt',
+      category: 'shirts',
+      image: '/images/d1.jpeg',
+      price: 75,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'White pants',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 65,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'Slim fit jeans',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 40,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'White pants',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 65,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'Trousers',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 40,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    }
+  ]
+}
