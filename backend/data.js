e57c51ceb160296262d8b892047c1a7d8244a313
manci <javiercaliescali@gmail.com>
2020-08-06 19:48:26 -0500

    create backend folder, and install node and babel in root folder

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+export default {
+  products: [
+    {
+      id: '1',
+      name: 'Slim shirt',
+      category: 'shirts',
+      image: '/images/d1.jpeg',
+      price: 55,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      id: '2',
+      name: ' Fit shirt',
+      category: 'shirts',
+      image: '/images/d1.jpeg',
+      price: 75,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      id: '3',
+      name: 'White pants',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 65,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      id: '4',
+      name: 'Slim fit jeans',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 40,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      id: '5',
+      name: 'White pants',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 65,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      id: '6',
+      name: 'Trousers',
+      category: 'pants',
+      image: '/images/d1.jpeg',
+      price: 40,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10
+    }
+  ]
+}
