fb42ebe002b63c3440f9e582a72905af20e399c6
manci <javiercaliescali@gmail.com>
2020-08-07 13:42:53 -0500

    resquest product detail to the server

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,61 +1,79 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen(props) {
-  const product = data.products.find( x => x.id === props.match.params.id )
-  return <div>
+  // const [qty, setQty] = useState(1);
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+
+    }
+  }, []); //  [] dispatch will run ufter component Didmount
+
+
+  return (
+  <div>
     <div className="back-to-results">
       <Link to="/">Back to results</Link>
     </div>
-    <div className="details">
-      <div className="details-image">
-        <img src={product.image} alt={product}></img>
-      </div>
-      <div className="details-info">
-        <ul>
-          <li>
-            <h4>{product.name}</h4>
-          </li>
-          <li>
-            {product.rating} Stars ({product.numReviews} Reviews)
-          </li>
-          <li>
-            <b>{product.price}</b>
-          </li>
-          <li>
-            Description:
-            <div>
-              {product.description}
+    {loading ? <div> Loading...</div> :
+      error ? <div>{error}</div> :
+        (
+          <div className="details">
+            <div className="details-image">
+              <img src={product.image} alt={product}></img>
             </div>
-          </li>
-        </ul>
-      </div>
-      <div className="details-action">
-        <ul>
-          <li>
-            Price: <b>${product.price} </b>
-          </li>
-          <li>
-            Status: {product.status}
-          </li>
-          <li>
-            Qty: <select>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </li>
-          <li>
-            <button className="button">Add to Cart</button>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-  </div>
+            <div className="details-info">
+              <ul>
+                <li>
+                  <h4>{product.name}</h4>
+                </li>
+                <li>
+                  {product.rating} Stars ({product.numReviews} Reviews)
+                </li>
+                <li>
+                  <b>{product.price}</b>
+                </li>
+                <li>
+                  Description:
+                  <div>
+                    {product.description}
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="details-action">
+              <ul>
+                <li>
+                  Price: <b>${product.price} </b>
+                </li>
+                <li>
+                  Status: {product.status}
+                </li>
+                <li>
+                  Qty: <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </li>
+                <li>
+                  <button className="button">Add to Cart</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )
+    }
+  </div>);
 }
 
 export default ProductScreen;
