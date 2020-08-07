470a8022461e24c29ed84fbbd1b29f03c4824076
manci <javiercaliescali@gmail.com>
2020-08-06 22:54:50 -0500

    install axios, define proxy in frontend, and fetch data from node server

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HomeScreen(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    }
+    fetchData();
+  })
+  
   return (
     <ul className="products">
             {
-              data.products.map(product =>           
-              <li>
+              products.map(product =>           
+              <li key={product.id}>
                 <div className="product">
                   <Link to={'product/'+ product.id}>
                     <img className="product-image" src={product.image} alt="product" />
