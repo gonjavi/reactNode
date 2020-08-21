0beec6ec016674f50b84b96f868dc067a348ac3e
manci <javiercaliescali@gmail.com>
2020-08-21 16:57:14 -0500

    crear modal creacion o edicion de producto

--- frontend/src/Screens/ProductsScreen.js
+++ frontend/src/Screens/ProductsScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { saveProduct } from '../actions/productActions';
+import { saveProduct, listProducts } from '../actions/productActions';
 
 function ProductsScreen(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [_id, setId] = useState('');
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [image, setImage] = useState('');
@@ -11,89 +13,143 @@ function ProductsScreen(props) {
   const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState('');
   const [description, setDescription] = useState('');
-  
+  const productList = useSelector(state => state.productList);
+  const { loading, products, error } = productList;
   const productSave = useSelector(state => state.productSave);
   const { loading: loadingSave, succes: successSave, error: errorSave } = productSave;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    
+    dispatch(listProducts());
     return () => {
 
     }
-  }, []);
+  }, []);  
+
+  const openModal = product => {
+    setModalVisible(true);
+    setId(product._id);
+    setName(product.name);
+    setPrice(product.price);
+    setImage(product.image);
+    setBrand(product.brand);
+    setCategory(product.category);
+    setCountInStock(product.countInStock); 
+  }
 
   const submitHandler = e => {
     e.preventDefault();
     dispatch(saveProduct({
+      _id: _id,
       name, price, image, brand, category,
       countInStock, description
     }));
   }
-
  
   return (
-  <div className="form">
-    <form onSubmit={submitHandler}>
-      <ul className="form-container">
-        <li>
-          <h2>Create Product</h2>
-        </li>
-        <li>
-          {loadingSave && <div>Loading...</div>}
-          {errorSave && <div>{errorSave}</div>}
-        </li>
-        <li>
-          <label htmlFor="name">
-            Name
-          </label>
-          <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
-        </li>
-        <li>
-          <label htmlFor="name">
-            Price
-          </label>
-          <input type="text" name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
-        </li>
-        <li>
-          <label htmlFor="image">
-           Image
-          </label>
-          <input type="text" name="image" id="image" onChange={(e) => setImage(e.target.value)} />
-        </li>
-        <li>
-          <label htmlFor="brand">
-            Brand
-          </label>
-          <input type="text" name="brand" id="brand" onChange={(e) => setBrand(e.target.value)} />
-        </li>
-        <li>
-          <label htmlFor="category">
-            Category
-          </label>
-          <input type="text" name="category" id="category" onChange={(e) => setCategory(e.target.value)} />
-        </li>
-        <li>
-          <label htmlFor="countInStock">
-            Count in Stock
-          </label>
-          <input type="text" name="countInStock" id="countInStock" onChange={(e) => setCountInStock(e.target.value)} />
-        </li>        
-        <li>
-          <label htmlFor="name">
-            Description
-          </label>
-          <textarea name="description" id="description" onChange={(e) => setDescription(e.target.value)} ></textarea>
-        </li>
-       
-        <li>
-          <button type="submit" className="button primary">Create</button>
-        </li>       
-      </ul>
-    </form>
-   
-  </div>);
+    <div className="content content-margined">
+      <div className="prodcut-header">
+        <h3>Products</h3>
+        <button onClick={() => openModal({})}>Create Product</button>
+      </div>
+
+      {modalVisible && 
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <ul className="form-container">
+            <li>
+              <h2>Create Product</h2>
+            </li>
+            <li>
+              {loadingSave && <div>Loading...</div>}
+              {errorSave && <div>{errorSave}</div>}
+            </li>
+            <li>
+              <label htmlFor="name">
+                Name
+              </label>
+              <input type="text" name="name" value={name} id="name" onChange={(e) => setName(e.target.value)} />
+            </li>
+            <li>
+              <label htmlFor="name">
+                Price
+              </label>
+              <input type="text" name="price" value={price}id="price" onChange={(e) => setPrice(e.target.value)} />
+            </li>
+            <li>
+              <label htmlFor="image">
+              Image
+              </label>
+              <input type="text" name="image" value={image} id="image" onChange={(e) => setImage(e.target.value)} />
+            </li>
+            <li>
+              <label htmlFor="brand">
+                Brand
+              </label>
+              <input type="text" name="brand" value={brand} id="brand" onChange={(e) => setBrand(e.target.value)} />
+            </li>
+            <li>
+              <label htmlFor="category">
+                Category
+              </label>
+              <input type="text" name="category" value={category} id="category" onChange={(e) => setCategory(e.target.value)} />
+            </li>
+            <li>
+              <label htmlFor="countInStock">
+                Count in Stock
+              </label>
+              <input type="text" name="countInStock" value={countInStock} id="countInStock" onChange={(e) => setCountInStock(e.target.value)} />
+            </li>        
+            <li>
+              <label htmlFor="name">
+                Description
+              </label>
+              <textarea name="description" value={description} id="description" onChange={(e) => setDescription(e.target.value)} ></textarea>
+            </li>
+          
+            <li>
+              <button type="submit" className="button primary">{_id?"Update":"Create"}</button>
+            </li> 
+            <li>
+              <button type="submit" onClick={() => setModalVisible(false)} className="button secondary">Back</button>
+            </li>         
+          </ul>
+        </form>
+      </div>
+      }
+      <div className="product-list">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Brand</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+            <tr>
+              <td>{product._id}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.category}</td>
+              <td>{product.brand}</td>
+              <td>
+                <button onClick={() => openModal(product)}>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>       
+    </div>
+
+  );
 }
 
 export default ProductsScreen;
