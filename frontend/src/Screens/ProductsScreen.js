1972f022b7ca0423b813219da00666ff4349684e
manci <javiercaliescali@gmail.com>
2020-08-21 09:23:04 -0500

    create products screen, create products routes

---
+++ frontend/src/Screens/ProductsScreen.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { signin } from '../actions/userActions';
+import productModel from '../../../backend/models/productModel';
+
+function ProductsScreen(props) {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
+  const [countInStock, setCountInStock] = useState('');
+  const [description, setDescription] = useState('');
+  const [rating, setRating] = useState('');
+  const [numReview, setnumReview] = useState('');
+ 
+  const productSave = useSelector(state => state.productSave);
+  const { loading: loadingSave, succes: successSave, error: errorSave } = productSave;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    
+    return () => {
+
+    }
+  }, [userInfo]);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(saveProduct({
+      name, price, image, brand, category,
+      countInStock, description, rating, numReview
+    }));
+  }
+
+ 
+  return (
+  <div className="form">
+    <form onSubmit={submitHandler}>
+      <ul className="form-container">
+        <li>
+          <h2>Create Product</h2>
+        </li>
+        <li>
+          {loading && <div>Loading...</div>}
+          {error && <div>{error}</div>}
+        </li>
+        <li>
+          <label htmlFor="name">
+            Name
+          </label>
+          <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="name">
+            Price
+          </label>
+          <input type="text" name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="image">
+           Image
+          </label>
+          <input type="text" name="image" id="image" onChange={(e) => setImage(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="brand">
+            Brand
+          </label>
+          <input type="text" name="brand" id="brand" onChange={(e) => setBrand(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="category">
+            Category
+          </label>
+          <input type="text" name="category" id="category" onChange={(e) => setCategory(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="countInStock">
+            Count in Stock
+          </label>
+          <input type="text" name="countInStock" id="countInStock" onChange={(e) => setCountInStock(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="rating">
+            Rating
+          </label>
+          <input type="text" name="rating" id="rating" onChange={(e) => setRating(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="price">
+            numReviews
+          </label>
+          <input type="text" name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="name">
+            Description
+          </label>
+          <textarea name="description" id="description" onChange={(e) => setDescription(e.target.value)} ></textarea>
+        </li>
+       
+        <li>
+          <button type="submit" className="button primary">Create</button>
+        </li>       
+      </ul>
+    </form>
+   
+  </div>);
+}
+
+export default ProductsScreen;
