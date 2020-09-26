4d503549b553cead25815413a23d0ba58b1f4668
manci <javiercaliescali@gmail.com>
2020-09-26 13:41:39 -0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,18 +28,6 @@ app.use((req, res, next) => {
 
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-/* app.get("/api/products/:id", (req, res) => {   
-  const productId = req.params.id;
-  const product = data.products.find(x => x.id === productId);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ msg: "Product Not Found." });
-  }
-});
 
-app.get("/api/products", (req, res) => {   
-  res.send(data.products);
-}); */
 
 app.listen(5000, () => { console.log("server started at http:://localhost:5000") });
